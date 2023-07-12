d816ff0b6a2c875b9fb0b8a2479cea6887c2535e
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-12 12:51:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 console.log('async');
 
-const pippo = 1;
-const pluto = 2;
-const paperino = pippo + pluto;
-const topolino = 'javascript'
+// function superFunzione() {
+//     console.log('sto eseguendo il codice dentro il timeout');
+// }
 
-console.log(topolino + paperino);
+// const pippo = 1;
+// const pluto = 2;
+
+// setTimeout(superFunzione, 5000);
+
+// setTimeout(() => {
+//     console.log('sto eseguendo il codice dentro il timeout');
+// }, 5000)
+
+// const paperino = pippo + pluto;
+// const topolino = 'javascript'
+
+// console.log(topolino + paperino);
+
+// setInterval(() => {
+//     console.log('sto eseguendo il codice dentro l\'intervallo');
+// }, 2000);
+
+// function gestisciRisposta(resp) {
+//     return resp.text();
+// }
+
+// function gestisciRisultatoFinale(res) {
+//     const dittoObj = JSON.parse(res);
+
+//     console.log(dittoObj);
+// }
+
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(gestisciRisposta).then(gestisciRisultatoFinale);
+fetch('https://pokeapi.co/api/v2/pokemon/ditto').then((pippo) => pippo.json()).then((pluto) => console.log(pluto));
